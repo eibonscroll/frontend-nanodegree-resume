3389f07869d23b2b0434f7b8eb1ebed4f722f90d
eibonscroll <mikepurvis7@gmail.com>
2016-02-01 00:03:52 -0600

    initial formatting changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace('%data%', "Michael Purvis")
+var formattedRole = HTMLheaderRole.replace('%data%', "Programmer")
+ //$('#main').append('Michael Purvis');
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+var skills = ["Fullstack", "FrontEnd", "Game Programming", "C++", "JavaScript"];
+
+var bio = {
+  name:"Michael Purvis",
+  role:"Programmer",
+  contact:"mikegpurvis@gmail.com",
+  pictureurl:"",
+  welcome:"Welcome",
+  skills:skills
+};
+
+var formattedContact = HTMLemail.replace('%data%', bio.contact);
+
+$('#main').append(formattedContact);
